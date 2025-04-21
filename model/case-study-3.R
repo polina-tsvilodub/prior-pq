8b578264929dcfc5471e75bd4849c29996ed7dc9
Robert Hawkins <hawkrobe@gmail.com>
2025-04-20 19:50:48 -0700

    add prior-pq to figure 3

--- model/case-study-3.R
+++ model/case-study-3.R
@@ -43,19 +43,6 @@ full_matrix <- priors %>%
 
 
 #######################################
-# 
-# policyAlpha = c(1,3,5,7,9)
-# questionerAlpha = c(1,3,5,7,9)
-# R1Alpha = c(1,3,5,7,9)
-# relevanceBetaR0 = c(0)
-# relevanceBetaR1 = c(0.1, 0.3, 0.5, 0.7, 0.9)
-# costWeight = c(0.1, 0.3, 0.5, 0.7, 0.9)
-# n_sample <- c(1,2,3,4,5)
-# questionCost <- c(0)
-# 
-# param_space <- expand_grid(policyAlpha, questionerAlpha, R1Alpha, relevanceBetaR0, relevanceBetaR1, costWeight, questionCost, scenarios, n_sample)
-
-##############################################
 
 run_model_tsos <- function (params, utils) {
   webPPL_data <- tibble('task' = "TSOS") %>% 
@@ -77,10 +64,9 @@ priorSampleParams <- function() {
     'policyAlpha'      = runif(1,min = 8.536908820191446, max = 8.536908820191446), # searched 0-10
     'questionerAlpha'  = runif(1,min = 5.892578142591768, max = 5.892578142591768), # searched 0-10
     'R1Alpha'          = runif(1,min = 2.9436908699427895, max = 2.9436908699427895), # searched 0-50
-    'relevanceBetaR0'  = runif(1,min = 0, max = 0), # fixed at 0
     'relevanceBetaR1'  = runif(1,min = 0.2907849349998428, max = 0.2907849349998428), # searched 0-1
     'costWeight'       = runif(1,min = 2.3421835313724717, max = 2.3421835313724717), # searched 0-5
-    'failure'          = runif(1,min = -5.936552662124935, max = -5.936552662124935), # searched -10 to 10
+    'failure'          = runif(1,min = -5.936552662124935, max = -5.936552662124935), # searched to 10
     'questionCost'     = runif(1,min = 0, max = 0) # fixed at 0
   )
   return(params)
@@ -88,7 +74,8 @@ priorSampleParams <- function() {
 
 
 empiricalPrior <- function(scenario) {
-  these_priors <- full_matrix %>% 
+  these_priors <- full_matrix %>%
+    mutate(targetOption = fct_relevel(targetOption, 'itemQuestion', 'competitor', 'sameCategory', 'otherCategory', 'mostSimilar')) %>%
     filter(itemName == scenario) %>%
     group_by(targetOption) %>%
     sample_n(1)
@@ -108,24 +95,14 @@ samples_each = 20
 scenarios_rep = rep(scenarios, samples_each)
 n_samples = length(scenarios_rep)
 
-
-param_search = FALSE
-
 plan(multisession, workers = 8)
 
-if (param_search == TRUE) {
-  n_samples = nrow(param_space)
-}
 priorPred <- furrr::future_map_dfr(1:n_samples, function(i) {
   message('run ', i)
-  if (param_search == TRUE) {
-    scenario <- param_space[i,]['scenarios'] %>% pull()
-    params <- param_space[i,] %>% select(-scenarios, -n_sample) %>% tibble()
-  } else {
-    scenario = scenarios_rep[i]
-    params <- priorSampleParams()
-  }
+  scenario = scenarios_rep[i]
+  params <- priorSampleParams()
   utils  <- empiricalPrior(scenario)
+  print(utils)
   out    <- tibble('run' = i) %>%
     cbind(params) %>%
     cbind(scenario) %>%
@@ -133,9 +110,6 @@ priorPred <- furrr::future_map_dfr(1:n_samples, function(i) {
     return (out)
 }, .progress = TRUE, .options = furrr_options(seed = 123))
 
-if (param_search == TRUE) {
-  write_csv(priorPred, here('data/priorpq/case_study_3/c3_parameter_search.csv'))
-} else {
-  write_csv(priorPred, here('data/priorpq/case_study_3/c3_model_preds_full.csv'))
-}
+write_csv(priorPred, here('data/priorpq/case_study_3/c3_model_preds_full.csv'))
+
 
